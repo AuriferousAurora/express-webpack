4d9ffc52a7b18faa3e44a050f179f11a02202dd8
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2020-01-23 00:33:14 -0600

    Initial commit for Express Webpack test project repository.

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const ssHelloWorld = require('ss-hello-world-module-incllc');
+
+const app = express(),
+            DIST_DIR = __dirname,
+            HTML_FILE = path.join(DIST_DIR, '/dist/index.html')
+            
+app.use(express.static(DIST_DIR))
+
+const hw = new ssHelloWorld;
+            
+app.get('/cs-hello-world', (req, res) => res.sendFile(HTML_FILE));
+app.get('/ss-hello-world', (req, res) => res.send(hw.getGreeting()));
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+    console.log(`Express application listening to ${port}...`);
+    console.log('Press Ctrl+C to terminate process.');
+});
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const csHelloWorld = require('cs-hello-world-module-incllc');
+
+const hw = new csHelloWorld;
+console.log(hw.getGreeting());
